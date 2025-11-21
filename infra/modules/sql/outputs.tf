037b69fe078adcfdb7f261ff4a92b4d92af8f123
Jiachen Tian <tjc990806@gmail.com>
2025-11-21 10:55:30 +0100

    new output

--- infra/modules/sql/outputs.tf
+++ infra/modules/sql/outputs.tf
@@ -12,3 +12,11 @@ output "primary_instance_name" {
   value       = google_sql_database_instance.primary.name
   description = "Primary Cloud SQL instance ID."
 }
+
+output "primary_private_ip" {
+  value = google_sql_database_instance.primary.private_ip_address
+}
+
+output "replica_private_ip" {
+  value = google_sql_database_instance.replica.private_ip_address
+}
